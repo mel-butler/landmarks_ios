a926e3c36ee331f180f56a02fca025678391fc52
Mel Butler <imelda52602@gmail.com>
2022-08-16 15:05:45 +0200

    WatchOS UI

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Mel Butler on 16/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
