45afb883a6deaffb88cc909fd7474170bb70d5bf
Mel Butler <imelda52602@gmail.com>
2022-08-16 10:40:56 +0200

    Profile Editor

---
+++ Landmarks/View/Profiles/ProfileHost.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Mel Butler on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
